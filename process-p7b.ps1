588ceae04fbf6330528839e34d6294229cb2d726
nechyo <iseullee7@gmail.com>
2024-12-05 10:58:25 +0900

    Create process-p7b.ps1

---
+++ process-p7b.ps1
@@ -0,0 +1,22 @@
+# output/*.p7b 파일을 blacklists/*.xml로 변환하는 스크립트
+
+$inputDirectory = "output"
+$outputDirectory = "blacklists"
+
+if (!(Test-Path $outputDirectory)) {
+    New-Item -ItemType Directory -Path $outputDirectory
+}
+
+$p7bFiles = Get-ChildItem -Path $inputDirectory -Filter "*.p7b"
+
+foreach ($file in $p7bFiles) {
+    $outputFileName = [System.IO.Path]::ChangeExtension($file.Name, ".xml")
+    $outputFilePath = Join-Path -Path $outputDirectory -ChildPath $outputFileName
+
+    try {
+        ConvertTo-WDACCodeIntegrityPolicy -BinaryFilePath $file.FullName -XmlFilePath $outputFilePath
+        Write-Host "Converted: $($file.FullName) -> $outputFilePath"
+    } catch {
+        Write-Error "Error converting file: $($file.FullName). $_"
+    }
+}
