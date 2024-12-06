40bba6c4864094b92b0e7434d4ac348513419d30
nechyo <iseullee7@gmail.com>
2024-12-06 15:22:04 +0900

    Update process-p7b.ps1

--- process-p7b.ps1
+++ process-p7b.ps1
@@ -1,4 +1,5 @@
-git clone https://github.com/nechyo/WDACTools & cd WDACTools
+git clone https://github.com/nechyo/WDACTools
+cd WDACTools
 Import-Module .\WDACTools.psm1
 cd ..
 # output/*.p7b 파일을 blacklists/*.xml로 변환하는 스크립트
